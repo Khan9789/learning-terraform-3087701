4c45e592959f9569d3832100fb0fe5ca1da5ddab
Khan9789 <154265068+Khan9789@users.noreply.github.com>
2025-04-18 20:15:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -38,8 +38,8 @@ resource "aws_security_group" "blog" {
 
 resource "aws_security_group_rule" "blog_http_in" {
   type        = "ingress"
-  from_port   = 80
-  to_port     = 80
+  from_port   = 22
+  to_port     = 22
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
   security_group_id = aws_security_group.blog.id
@@ -48,8 +48,8 @@ resource "aws_security_group_rule" "blog_http_in" {
 
 resource "aws_security_group_rule" "blog_https_in" {
   type        = "ingress"
-  from_port   = 443
-  to_port     = 443
+  from_port   = 8080
+  to_port     = 8080
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
   security_group_id = aws_security_group.blog.id
